c224c11689282ad55fd8973069c860e1377cb804
Tony Baker <this.tonybaker@gmail.com>
2019-08-19 18:29:11 +0000

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,10 +5,12 @@ class SessionsController < ApplicationController
   end
 
   def create
-
+    redirect_to action: 'new' if params[:name].nil? || params[:name].empty?
+    session[:name] = params[:name]
+    redirect_to '/'
   end
 
   def destroy
-
+    session.delete :name
   end
 end
